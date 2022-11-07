7755b703114b55c3363baa79ea9972472f53b8d2
Matthew Archer <ma595@cam.ac.uk>
2022-11-07 14:26:15 +0000

    style fixes

--- icelake-spack-env.sh
+++ icelake-spack-env.sh
@@ -2,9 +2,9 @@
 # ssh to icelake
 ssh ma595@login-q-2.hpc.cam.ac.uk
 
-# start from fresh shell. Do not purge environment. Here we allocate compute node:
+# Start from fresh shell. Do not purge environment. Here we allocate compute node:
 # sintr -p icelake -N 1 --exclusive -t 1:00:0 --pty bash
-srun  -p icelake -N 1 --exclusive -t 1:00:0  --pty bash
+srun -p icelake -N 1 --exclusive -t 1:00:0  --pty bash
 
 # set up environment
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git spack
@@ -21,7 +21,7 @@ spack:
       enable:
       - tcl
       roots:
-        tcl: /rds/project/rds-5mCMIDBOkPU/ma595/fenics/spack
+        tcl: /rds/project/rds-5mCMIDBOkPU/ma595/fenics/spack-module
   specs:
   - re2c
   - py-fenics-dolfinx@main%gcc@11.3.0 ^intel-oneapi-mpi@2021.6.0%gcc@11.3.0 ^petsc@main
@@ -29,7 +29,7 @@ spack:
   # - fenics-performance-test@main
   view:
     icelake:
-      root: /rds/project/rds-5mCMIDBOkPU/ma595/fenics/spack
+      root: /rds/project/rds-5mCMIDBOkPU/ma595/fenics/spack-view
       link_type: symlink
   packages:
     all:
@@ -42,11 +42,11 @@ spack:
       buildable: false
 EOF
 
-# activate and concretize
+# Activate and concretize
 spack env activate icl-test
 spack concretize -f
-# change intel-oneapi-mpi underlying compiler to gcc (fails on mumps with intel compiler - cannot import metis.h?).
-# switch to gcc as we use gcc everywhere now.
+# Change intel-oneapi-mpi underlying compiler to gcc (fails on mumps with intel compiler - cannot import metis.h?).
+# Switch to gcc as we use gcc everywhere now.
 # TODO: consider adding to spack.yaml
 export I_MPI_CC=gcc
 export I_MPI_CXX=g++
@@ -55,6 +55,6 @@ export I_MPI_F90=gfortran
 export I_MPI_FC=gfortran
 # this will now fail on ninja - but let it install everything
 spack install
-# now load re2c
+# now load re2c into path
 spack load re2c
 spack install
